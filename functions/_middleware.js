88918f2d558d43ad0562f1fbeb7b510f76127c9d
adangadong <127275159+adangadong@users.noreply.github.com>
2026-07-16 14:24:06 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,10 +1,4 @@
-// 全局中间件：为所有 API 请求添加基本的安全响应头
-export const onRequest = async (context) => {
-  const response = await context.next();
-  
-  // 设置响应头，防止别人通过 iframe 嵌套你的网站，并允许安全的 API 交互
-  response.headers.set("X-Frame-Options", "DENY");
-  response.headers.set("Content-Type", "application/json;charset=utf-8");
-  
-  return response;
-};
+export async function onRequest(context) {
+  // 临时直接放行所有请求，不拦截
+  return await context.next();
+}
